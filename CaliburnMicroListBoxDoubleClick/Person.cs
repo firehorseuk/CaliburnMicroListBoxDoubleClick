d8e0a09e23be0c0f850adb361be7c3c9138a446e
Alan Lam <alanlamuk@yahoo.co.uk>
2021-02-13 13:53:32 +0000

    Add project files.

---
+++ CaliburnMicroListBoxDoubleClick/Person.cs
@@ -0,0 +1,43 @@
+﻿using Caliburn.Micro;
+
+namespace CaliburnMicroListBoxDoubleClick
+{
+    public class Person : PropertyChangedBase
+    {
+        private string _FirstName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                _FirstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+            }
+        }
+
+        private string _LastName;
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                _LastName = value;
+                NotifyOfPropertyChange(() => LastName);
+            }
+        }
+
+        private string _City;
+
+        public string City
+        {
+            get { return _City; }
+            set
+            {
+                _City = value;
+                NotifyOfPropertyChange(() => City);
+            }
+        }
+    }
+}
---
+++ CaliburnMicroListBoxDoubleClick/ShellViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CaliburnMicroListBoxDoubleClick
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        private List<Person> persons;
+
+        public ShellViewModel()
+        {
+            persons = new List<Person>();
+            persons.Add(new Person() { FirstName = "Arie", LastName = "Jones", City = "Indianapolis" });
+            persons.Add(new Person() { FirstName = "Brett", LastName = "Canova", City = "Indianapolis" });
+            persons.Add(new Person() { FirstName = "Justin", LastName = "Bieber", City = "Somewhere in Canada" });
+            persons.Add(new Person() { FirstName = "Steve", LastName = "Jones", City = "Denver" });
+            persons.Add(new Person() { FirstName = "Dennis", LastName = "Miller", City = "Santa Barbara" });
+            persons.Add(new Person() { FirstName = "David", LastName = "Letterman", City = "New York" });
+
+            HereList = new ObservableCollection<Person>(persons);
+            ThereList = new ObservableCollection<Person>();
+        }
+
+        private ObservableCollection<Person> _HereList;
+
+        public ObservableCollection<Person> HereList
+        {
+            get
+            {
+                return _HereList;
+            }
+            set
+            {
+                _HereList = value;
+                NotifyOfPropertyChange(() => HereList);
+            }
+        }
+
+        private ObservableCollection<Person> _ThereList;
+
+        public ObservableCollection<Person> ThereList
+        {
+            get
+            {
+                return _ThereList;
+            }
+            set
+            {
+                _ThereList = value;
+                NotifyOfPropertyChange(() => ThereList);
+            }
+        }
+
+        public void MoveToThere(Person person)
+        {
+            HereList.Remove(person);
+            ThereList.Add(person);
+        }
+
+        public void MoveToHere(Person person)
+        {
+            HereList.Add(person);
+            ThereList.Remove(person);
+        }
+    }
+}
